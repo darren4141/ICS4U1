2b7b91b580f88cef5d0968a51a2b82edd6a10645
Darren Liu <109255883+darren4141@users.noreply.github.com>
2024-02-06 15:20:52 -0500

    added more methods

--- L1ArrayExercise.java
+++ L1ArrayExercise.java
@@ -7,41 +7,8 @@ public class L1ArrayExercise{
     public static void main(String[] args) throws IOException{
 
         System.out.println("Welcome to the array builder program");
-        System.out.println("What is the size of your array?");
-        int n = Integer.parseInt(br.readLine());
-        int[] numArray = new int[n];
-        int sum = 0;
+        int [] numArray = buildArray();
 
-        //loop through the size of the array and fill it with user input
-        for(int i = 0; i < n; i++){
-            System.out.println("Enter number " + (i + 1));
-            numArray[i] = Integer.parseInt(br.readLine());
-        }
-
-        printArray(numArray);
-
-        //loop through the array and use an accumulator to keep track of the sum
-        for(int i = 0; i < n; i++){
-            sum+= numArray[i];
-        }
-
-        System.out.println();
-        System.out.println("The sum is " + sum);
-        System.out.println();
-
-        System.out.println("The sorted array is:");
-
-        for(int i = numArray.length - 1; i > 0; i--){
-            for(int j = 0; j < i; j++){
-                if(numArray[j] > numArray[j+1]){
-                    int temp = numArray[j];
-                    numArray[j] = numArray[j+1];
-                    numArray[j+1] = temp;
-                }
-            }
-        }
-
-        printArray(numArray);
 
     }
 
@@ -56,4 +23,107 @@ public class L1ArrayExercise{
         System.out.print(arr[n-1] + "]");
         System.out.println();
     }
+
+    public static void printArrayRev(int [] arr){
+        System.out.print("[");
+        int n = arr.length;
+        for(int i = n-1; i > 0; i--){
+            System.out.print(arr[i] + ", ");
+        }
+
+        System.out.print(arr[0] + "]");
+        System.out.println();
+    }
+
+    public static double average(int [] arr){
+        return (double)(sumArray(arr) / arr.length);
+    }
+
+    public static int maxValue(int [] arr){
+        int max = arr[0];
+        for(int i = 0; i < arr.length; i++){
+            if(arr[i] > max){
+                max = arr[i];
+            }
+        }
+        return max;
+    }
+
+    public static int minValue(int [] arr){
+        int min = arr[0];
+        for(int i = 0; i < arr.length; i++){
+            if(arr[i] < min){
+                min = arr[i];
+            }
+        }
+        return min;
+    }
+
+    public static int[] search(int [] arr, int num){
+        int frequency = 0;
+
+        for(int i = 0; i < arr.length; i++){
+            if(arr[i] == num) frequency++;
+        }
+
+        int [] positions = new int[frequency];
+        int count = 0;
+
+        for(int i = 0; i < arr.length; i++){
+            if(arr[i] == num){
+                positions[count] = i;
+                count++;
+            }
+        }
+        return positions;
+    }
+
+    public static void sortArray(int[]arr){
+        for(int i = arr.length - 1; i > 0; i--){
+            for(int j = 0; j < i; j++){
+                if(arr[j] > arr[j+1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+    }
+
+    public static int sumArray(int[]arr){
+        int sum = 0;
+        for(int i = 0; i < arr.length; i++){
+            sum+=arr[i];
+        }
+
+        return sum;
+    }
+
+    public static int[] buildArray() throws IOException{
+        System.out.println("What is the size of your array?");
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+
+        //loop through the size of the array and fill it with user input
+        for(int i = 0; i < n; i++){
+            System.out.println("Enter number " + (i + 1));
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        return arr;
+
+    }
+
+    public static void countPos(int [] arr){
+        int count = 0;
+        for(int i = 0; i < arr.length; i++){
+            if(arr[i] > 0 ){
+                count++;
+            }
+        }
+    }
+
+
+
+
 }
