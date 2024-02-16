14d275699e0ec1b3cca7631ec90d9f9ca4bb93a0
Darren Liu <109255883+darren4141@users.noreply.github.com>
2024-02-16 10:57:24 -0500

    2d array

--- L4TwoDArrays.java
+++ L4TwoDArrays.java
@@ -3,8 +3,22 @@ import java.io.*;
 public class L4TwoDArrays {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
+    public static void main(String[] args)throws IOException{
+        L4TwoDArrays TwoDArray = new L4TwoDArrays();
+        System.out.println("welcome!");
 
-    public int[][] build2dArray() throws IOException{
+        int[][] numsColRow = TwoDArray.build2dArrayColRow();
+        TwoDArray.print2dArray(numsColRow);
+
+        int[][] numsRowCol = TwoDArray.build2dArrayRowCol();
+        TwoDArray.print2dArray(numsRowCol);
+    }
+
+    /*
+     * Method builds a 2d array by inputting rows first
+     * @return: int[][] nums --> the built array
+     */
+    public int[][] build2dArrayColRow() throws IOException{
         System.out.println("How many columns does your array have?");
         int col = Integer.parseInt(br.readLine());
         
@@ -21,5 +35,42 @@ public class L4TwoDArrays {
         }
 
         return nums;
+    }
+
+    /*
+     * Method builds a 2d array by inputting columns first
+     * @return: int[][] nums --> the built array
+     */
+    public int[][] build2dArrayRowCol()throws IOException{
+        System.out.println("How many rows does your array have?");
+        int row = Integer.parseInt(br.readLine());
+        
+        System.out.println("How many columns does your array have?");
+        int col = Integer.parseInt(br.readLine());
+
+        int[][] nums = new int[col][row];
+
+        for(int i = 0; i < row; i++){
+            for(int j = 0; j < col; j++){
+                System.out.println("Enter value located at column: " + i + " and row: " + j);
+                nums[j][i] = Integer.parseInt(br.readLine());
+            }
+        }
+
+        return nums;
+    }
+
+    /*
+     * Method that prints out a 2D array in a formatted manner
+     * @param: int[][] arr --> the array to be printed
+     */
+    public void print2dArray(int[][] arr){
+        for(int i = 0; i < arr.length; i++){
+            System.out.print("|[" + arr[i][0]);
+            for(int j = 1; j < arr[i].length; j++){
+                System.out.print(", " + arr[i][j]);
+            }
+            System.out.println("]|");
+        }
     }
 }
