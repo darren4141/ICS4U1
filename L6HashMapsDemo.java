9a0ce0a9c95daa2a9b4caeb45eb8c5690956bfb9
Darren Liu <109255883+darren4141@users.noreply.github.com>
2024-03-06 10:31:26 -0500

    a

--- L6HashMapsDemo.java
+++ L6HashMapsDemo.java
@@ -20,6 +20,25 @@ public class L6HashMapsDemo {
         for(String v : capitalCities.keySet()){
             System.out.println(v + " : " + capitalCities.get(v));
         }
+
+        HashMap<String, Integer> people = new HashMap<String, Integer>();
+        people.put("John", 32);
+        people.put("Steve", 30);
+        people.put("Angie", 33);
+
+        for(String v : people.keySet()){
+            System.out.println(v + " : " + people.get(v));
+        }
+    
+    }
+
+    public static String getName(HashMap<String, Integer> list, int size, int age){
+        for(String v : list.keySet()){
+            if(list.get(v) == age){
+                return v;
+            }
+        }
+        return null;
     }
 
 }
