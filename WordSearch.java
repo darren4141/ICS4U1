d98edeeed563328656c751c30f66fa303d61d41b
Darren Liu <109255883+darren4141@users.noreply.github.com>
2024-03-25 11:16:14 -0400

    word search

--- WordSearch.java
+++ WordSearch.java
@@ -1,4 +1,6 @@
 import java.awt.Container;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -6,41 +8,77 @@ import java.awt.event.ActionListener;
 import javax.swing.*;
 
 public class WordSearch extends JFrame implements ActionListener{
+    static final int ROWS = 6;
+    static final int COLS = 6;
     static JLabel mainMessage = new JLabel("");
 
     public WordSearch(){
         setTitle("Word Search");
         setSize(500, 500);
 
-        GridLayout wordGrid = new GridLayout();
-        wordGrid.setColumns(6);
-        wordGrid.setRows(6);
+        FlowLayout containerLayout = new FlowLayout();
+        containerLayout.setAlignment(FlowLayout.LEFT);
+        GridLayout wordGridLayout = new GridLayout();
+        wordGridLayout.setColumns(COLS);
+        wordGridLayout.setRows(ROWS);
 
-        JPanel wordPanel = new JPanel();
-        // wordPanel.setSize(100, 100);
-        wordPanel.setBounds(0, 0, 100, 100);
-        wordPanel.setLayout(wordGrid);
+        JPanel containerPanel = new JPanel();
+        containerPanel.setLayout(containerLayout);
 
-        for(int i = 0; i < 36; i++){
-            wordPanel.add(new JLabel("a"));
+        JPanel wordGridPanel = new JPanel();
+        wordGridPanel.setPreferredSize(new Dimension(100, 100));
+        wordGridPanel.setLayout(wordGridLayout);
+
+        char[][] wordGrid = fillTwoDArray(ROWS, COLS);
+
+        for(int i = 0; i < ROWS; i++){
+            for(int j = 0; j < COLS; j++){
+                wordGridPanel.add(new JLabel(Character.toString(wordGrid[i][j])));
+            }
         }
 
         Container contentPane = getContentPane();
-        contentPane.add(wordPanel);
+        containerPanel.add(wordGridPanel);
+        contentPane.add(containerPanel);
 
         //configure settings
         setVisible(true);
-        setResizable(false);
+        setResizable(true);
         
     }
 
     public static void main(String[] args){
-        WordSearch word = new WordSearch();
-
+        WordSearch wordSearch = new WordSearch();
     }
     
     public void actionPerformed(ActionEvent event){
         String command = event.getActionCommand();
 
+    }
+
+    public static char[][] fillTwoDArray(int rows, int cols){
+        char[][] grid = new char[rows][cols];
+        int[] letterFrequencies = {43, 11, 23, 17, 57, 9, 13, 15, 38, 1, 6, 28, 15, 34, 37, 16, 1, 39, 29, 35, 19, 5, 7, 1, 9, 1};
+        int sumFrequencies = 0;
+        for(int f : letterFrequencies){
+            sumFrequencies += f;
+        }
+        char[] letters = new char[sumFrequencies];
+        
+        int mark = 0;
+
+        for(int i = 0; i < letterFrequencies.length; i++){
+            for(int j = 0; j < letterFrequencies[i]; j++){
+                letters[mark + j] = (char)(65+i);
+            }
+            mark += letterFrequencies[i];
+        }
+
+        for(int i = 0; i < rows; i++){
+            for(int j = 0; j < cols; j++){
+                grid[i][j] = letters[(int)(Math.random() * sumFrequencies)];
+            }
+        }
+        return grid;
     }
 }
