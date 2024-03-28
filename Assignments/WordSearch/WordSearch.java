bffab8c9828525622b1dc65dbd212830565c389d
Darren Liu <109255883+darren4141@users.noreply.github.com>
2024-03-28 17:22:24 -0400

    more features

--- Assignments/WordSearch/WordSearch.java
+++ Assignments/WordSearch/WordSearch.java
@@ -30,9 +30,16 @@ public class WordSearch extends JFrame implements ActionListener{
     static final int FILELINES = 109582;
     static JPanel wordGridPanel;
     static JPanel horizontalFrame12;
+    static JPanel horizontalFrame22;
+    static JPanel horizontalFrame23;
+    static JPanel horizontalFrame252;
+    static JLabel scorePrompt;
+    static int totalScore = 0;
+    static boolean cheatsOn = false;
     static int[] trackedWordStart = new int[2];
     static int[] trackedWordEnd = new int[2];
     static ArrayList<String> foundWords = new ArrayList<String>();
+    static ArrayList<String> answers = new ArrayList<String>();
     static int [][] possibleDirections = {
         {-1, -1},
         {-1, 0},
@@ -43,6 +50,7 @@ public class WordSearch extends JFrame implements ActionListener{
         {1, 0},
         {1, 1}
     };
+    static int[] letterFrequencies = {43, 11, 23, 17, 57, 9, 13, 15, 38, 1, 6, 28, 15, 34, 37, 16, 1, 39, 29, 35, 19, 5, 7, 1, 9, 1};
 
     public WordSearch(){
         setTitle("Word Search");
@@ -51,19 +59,25 @@ public class WordSearch extends JFrame implements ActionListener{
         JPanel container = new JPanel();
         BoxLayout containerFrame = new BoxLayout(container, BoxLayout.X_AXIS);
         container.setLayout(containerFrame);
-        container.setBorder(BorderFactory.createLineBorder(Color.black));
+        // container.setBorder(BorderFactory.createLineBorder(Color.black));
 
         JPanel verticalFrame1 = new JPanel();
         BoxLayout verticalFrame1Layout = new BoxLayout(verticalFrame1, BoxLayout.Y_AXIS);
         verticalFrame1.setMaximumSize(new Dimension(260, SCREENHEIGHT));
         verticalFrame1.setLayout(verticalFrame1Layout);
-        verticalFrame1.setBorder(BorderFactory.createLineBorder(Color.orange));
+        // verticalFrame1.setBorder(BorderFactory.createLineBorder(Color.orange));
 
         JPanel verticalFrame2 = new JPanel();
         BoxLayout verticalFrame2Layout = new BoxLayout(verticalFrame2, BoxLayout.Y_AXIS);
-        verticalFrame2.setMaximumSize(new Dimension(260, SCREENHEIGHT));
+        verticalFrame2.setMaximumSize(new Dimension(150, SCREENHEIGHT));
         verticalFrame2.setLayout(verticalFrame2Layout);
-        verticalFrame2.setBorder(BorderFactory.createLineBorder(Color.red));
+        // verticalFrame2.setBorder(BorderFactory.createLineBorder(Color.red));
+
+        JPanel verticalFrame25 = new JPanel();
+        BoxLayout verticalFrame25Layout = new BoxLayout(verticalFrame25, BoxLayout.Y_AXIS);
+        verticalFrame25.setMaximumSize(new Dimension(150, SCREENHEIGHT));
+        verticalFrame25.setLayout(verticalFrame25Layout);
+        // verticalFrame25.setBorder(BorderFactory.createLineBorder(Color.blue));
 
         JPanel horizontalFrame0 = new JPanel();
         FlowLayout frame0Layout = new FlowLayout();
@@ -84,7 +98,7 @@ public class WordSearch extends JFrame implements ActionListener{
         
         horizontalFrame1.setLayout(frame1Layout);
         horizontalFrame1.setMaximumSize(new Dimension(SCREENWIDTH, 260));
-        horizontalFrame1.setBorder(BorderFactory.createLineBorder(Color.red));
+        // horizontalFrame1.setBorder(BorderFactory.createLineBorder(Color.red));
         
         GridLayout wordGridLayout = new GridLayout();
         wordGridLayout.setColumns(COLS);
@@ -107,7 +121,7 @@ public class WordSearch extends JFrame implements ActionListener{
         FlowLayout frame2Layout = new FlowLayout();
         frame2Layout.setAlignment(FlowLayout.LEFT);
         horizontalFrame2.setLayout(frame2Layout);
-        horizontalFrame2.setBorder(BorderFactory.createLineBorder(Color.blue));
+        // horizontalFrame2.setBorder(BorderFactory.createLineBorder(Color.blue));
         horizontalFrame2.setMaximumSize(new Dimension(SCREENWIDTH, 40));
         guessTextField.setColumns(9);
         JButton guessButton = new JButton("Guess!");
@@ -120,10 +134,19 @@ public class WordSearch extends JFrame implements ActionListener{
         FlowLayout frame3Layout = new FlowLayout();
         frame3Layout.setAlignment(FlowLayout.LEFT);
         horizontalFrame3.setLayout(frame3Layout);
-        horizontalFrame3.setBorder(BorderFactory.createLineBorder(Color.green));
+        // horizontalFrame3.setBorder(BorderFactory.createLineBorder(Color.green));
         horizontalFrame3.setMaximumSize(new Dimension(SCREENWIDTH, 30));
         guessPrompt.setText("Make a guess!");
         horizontalFrame3.add(guessPrompt);
+
+        JPanel horizontalFrame4 = new JPanel();
+        FlowLayout frame4Layout = new FlowLayout();
+        frame4Layout.setAlignment(FlowLayout.LEFT);
+        horizontalFrame4.setLayout(frame4Layout);
+        horizontalFrame3.setMaximumSize(new Dimension(SCREENWIDTH, 30));
+        JButton cheatButton = new JButton("Cheat!");
+        cheatButton.addActionListener(this);
+        horizontalFrame4.add(cheatButton);
 
         JPanel horizontalFrame11 = new JPanel();
         FlowLayout frame11Layout = new FlowLayout();
@@ -134,30 +157,43 @@ public class WordSearch extends JFrame implements ActionListener{
         answersPrompt.setFont(new Font("Sans Serif", Font.BOLD, 18));
         horizontalFrame11.add(answersPrompt);
 
+        JPanel horizontalFrame251 = new JPanel();
+        FlowLayout frame251Layout = new FlowLayout();
+        frame251Layout.setAlignment(FlowLayout.LEFT);
+        horizontalFrame251.setLayout(frame251Layout);
+        horizontalFrame251.setMaximumSize(new Dimension(SCREENWIDTH, 30));
+        scorePrompt = new JLabel("Score: " + totalScore);
+        scorePrompt.setFont(new Font("Sans Serif", Font.BOLD, 20));
+        answersPrompt.setFont(new Font("Sans Serif", Font.BOLD, 20));
+        horizontalFrame251.add(scorePrompt);
+
         horizontalFrame12 = new JPanel();
-        GridLayout horizontalFrame12Layout = new GridLayout(1000, 1);
+        GridLayout horizontalFrame12Layout = new GridLayout(10000, 1);
         horizontalFrame12.setLayout(horizontalFrame12Layout);
+
+        horizontalFrame252 = new JPanel();
+        GridLayout horizontalFrame252Layout = new GridLayout(10000, 1);
+        horizontalFrame252.setLayout(horizontalFrame252Layout);
 
         JPanel verticalFrame3 = new JPanel();
         GridLayout verticalFrame3Layout = new GridLayout(1, 1);
         verticalFrame3.setMaximumSize(new Dimension(260, SCREENHEIGHT));
         verticalFrame3.setLayout(verticalFrame3Layout);
-        verticalFrame3.setBorder(BorderFactory.createLineBorder(Color.green));
+        // verticalFrame3.setBorder(BorderFactory.createLineBorder(Color.green));
 
-        JPanel horizontalFrame22 = new JPanel();
+        horizontalFrame22 = new JPanel();
         BoxLayout horizontalFrame22Layout = new BoxLayout(horizontalFrame22, BoxLayout.Y_AXIS);
-        horizontalFrame22.setMaximumSize(new Dimension(260, SCREENHEIGHT));
+        horizontalFrame22.setMaximumSize(new Dimension(130, SCREENHEIGHT));
         horizontalFrame22.setLayout(horizontalFrame22Layout);
-        horizontalFrame22.setBorder(BorderFactory.createLineBorder(Color.black));
+        // horizontalFrame22.setBorder(BorderFactory.createLineBorder(Color.black));
 
-        ArrayList<String> answers = findAnswers(wordGrid);
-        JLabel answerTitle = new JLabel("ANSWERS:");
-        answerTitle.setFont(new Font("Sans-Serif", Font.BOLD ,15));
-        horizontalFrame22.add(answerTitle);
-        System.out.println(answers);
-        for(String a : answers){
-            horizontalFrame22.add(new JLabel(a, SwingConstants.LEFT));
-        }
+        horizontalFrame23 = new JPanel();
+        BoxLayout horizontalFrame23Layout = new BoxLayout(horizontalFrame23, BoxLayout.Y_AXIS);
+        horizontalFrame23.setMaximumSize(new Dimension(130, SCREENHEIGHT));
+        horizontalFrame23.setLayout(horizontalFrame23Layout);
+        // horizontalFrame23.setBorder(BorderFactory.createLineBorder(Color.black));
+
+        answers = findAnswers(wordGrid);
 
         JOptionPane.showMessageDialog(this, "a");
 
@@ -166,11 +202,16 @@ public class WordSearch extends JFrame implements ActionListener{
         verticalFrame1.add(horizontalFrame1);
         verticalFrame1.add(horizontalFrame2);
         verticalFrame1.add(horizontalFrame3);
+        verticalFrame1.add(horizontalFrame4);
         verticalFrame2.add(horizontalFrame11);
         verticalFrame2.add(horizontalFrame12);
+        verticalFrame25.add(horizontalFrame251);
+        verticalFrame25.add(horizontalFrame252);
         verticalFrame3.add(horizontalFrame22);
+        verticalFrame3.add(horizontalFrame23);
         container.add(verticalFrame1);
         container.add(verticalFrame2);
+        container.add(verticalFrame25);
         container.add(verticalFrame3);
         contentPane.add(container);
         container.getRootPane().setDefaultButton(guessButton);
@@ -191,6 +232,12 @@ public class WordSearch extends JFrame implements ActionListener{
         }
 
         fileReader.close();
+
+        for(int i = 0; i < letterFrequencies.length; i++){
+            letterFrequencies[i] -= ((letterFrequencies[i]-1) * (letterFrequencies[i]-1)) * 0.005;
+            letterFrequencies[i] *= 0.3;
+            letterFrequencies[i]++;
+        }
 
         WordSearch wordSearch = new WordSearch();
     }
@@ -230,26 +277,35 @@ public class WordSearch extends JFrame implements ActionListener{
                     guessPrompt.setText("You already entered this!");
                     guessPrompt.setForeground(Color.red);
                     horizontalFrame12.removeAll();
+                    horizontalFrame252.removeAll();
                     if(!foundWords.contains(guess)){
                         foundWords.add(guess);
                         guessPrompt.setText("YES, AND IN DICTIONARY");
                         guessPrompt.setForeground(new Color(33, 168, 7));
+                        int score = 0;
+                        for(int j = 0; j < guess.length(); j++){
+                            score += (30 - letterFrequencies[(int)guess.charAt(j)-65]) * 1.5;
+                        }
+                        score *= guess.length();
+                        totalScore += score;
+                        for(int i = 0; i < foundWords.size(); i++){
+                            int thisScore = 0;
+                            for(int j = 0; j < foundWords.get(i).length(); j++){
+                                thisScore += (30 - letterFrequencies[(int)foundWords.get(i).charAt(j)-65]) * 1.5;
+                            }
+                            thisScore *= foundWords.get(i).length();
+                            JLabel scoreLabel = new JLabel(Integer.toString(thisScore));
+                            JLabel word = new JLabel(foundWords.get(i));
+                            word.setFont(new Font("Sans-Serif", Font.BOLD ,15));
+                            scoreLabel.setFont(new Font("Sans-Serif", Font.BOLD ,15));
+                            scoreLabel.setHorizontalAlignment(SwingConstants.LEFT);
+                            word.setHorizontalAlignment(SwingConstants.LEFT);
+                            horizontalFrame12.add(word);
+                            horizontalFrame252.add(scoreLabel);
+                        }
                     }
                     sortArrayList(foundWords);
-                    for(int i = 0; i < foundWords.size(); i++){
-                        JLabel word = new JLabel(foundWords.get(i));
-                        word.setFont(new Font("Sans-Serif", Font.BOLD ,15));
-                        word.setHorizontalAlignment(SwingConstants.LEFT);
-                        // JPanel wordPanel= new JPanel();
-                        // wordPanel.setMaximumSize(new Dimension(SCREENWIDTH/2, 30));
-                        // FlowLayout layout = new FlowLayout();
-                        // layout.setAlignment(FlowLayout.LEFT);
-                        // wordPanel.setLayout(layout);
-                        // wordPanel.add(word);
-                        // wordPanel.setBorder(BorderFactory.createLineBorder(Color.pink));
-                        horizontalFrame12.add(word);
-                    }
-                    horizontalFrame12.add(new JLabel(" "));
+                    scorePrompt.setText("Score: " + totalScore);
                 }else{
                     guessPrompt.setText("YES, NOT IN DICTIONARY");
                     guessPrompt.setForeground(Color.red);
@@ -265,15 +321,43 @@ public class WordSearch extends JFrame implements ActionListener{
         }
 
         if(command.equals("Refresh")){
+            guessPrompt.setText("Enter a word!");
+            guessPrompt.setForeground(Color.black);
             wordGrid = fillTwoDArray(ROWS, COLS);
             foundWords.clear();
             horizontalFrame12.removeAll();
+            horizontalFrame252.removeAll();
             trackedWordStart[0] = 0;
             trackedWordStart[1] = 0;
             trackedWordEnd[0] = 0;
             trackedWordEnd[1] = 0;
+            totalScore = 0;
+            answers.clear();
+            answers = findAnswers(wordGrid);
+            horizontalFrame22.removeAll();
+            horizontalFrame23.removeAll();
+            if(cheatsOn){
+                displayAnswer();
+            }
             wordGridPanel.revalidate();
             wordGridPanel.repaint(); 
+            horizontalFrame12.revalidate();
+            horizontalFrame12.repaint();
+            horizontalFrame252.revalidate();
+            horizontalFrame252.repaint();
+            horizontalFrame22.revalidate();
+            horizontalFrame22.repaint();
+            horizontalFrame23.revalidate();
+            horizontalFrame23.repaint();
+        }
+
+        if(command.equals("Cheat!")){
+            if(cheatsOn){
+                hideAnswer();
+            }else{
+                displayAnswer();
+            }
+            cheatsOn = !cheatsOn;
         }
 
         printWordGrid(fileContainsWord);
@@ -281,6 +365,8 @@ public class WordSearch extends JFrame implements ActionListener{
         wordGridPanel.repaint(); 
         horizontalFrame12.revalidate();
         horizontalFrame12.repaint();
+        horizontalFrame252.revalidate();
+        horizontalFrame252.repaint();
     }
 
     public static char[][] fillTwoDArray(int rows, int cols){
@@ -288,12 +374,6 @@ public class WordSearch extends JFrame implements ActionListener{
         int consonantCount = 0;
 
         char[][] grid = new char[rows][cols];
-        int[] letterFrequencies = {43, 11, 23, 17, 57, 9, 13, 15, 38, 1, 6, 28, 15, 34, 37, 16, 1, 39, 29, 35, 19, 5, 7, 1, 9, 1};
-        for(int i = 0; i < letterFrequencies.length; i++){
-            letterFrequencies[i] -= ((letterFrequencies[i]-1) * (letterFrequencies[i]-1)) * 0.005;
-            letterFrequencies[i] *= 0.3;
-            letterFrequencies[i]++;
-        }
         int sumFrequencies = 0;
         for(int f : letterFrequencies){
             sumFrequencies += f;
@@ -472,6 +552,27 @@ public class WordSearch extends JFrame implements ActionListener{
         }
     }
     
+    public static void displayAnswer(){
+        JLabel answerTitle = new JLabel("ANSWERS:");
+        answerTitle.setFont(new Font("Sans-Serif", Font.BOLD ,15));
+        JLabel fillerTitle = new JLabel(" ");
+        fillerTitle.setFont(new Font("Sans-Serif", Font.BOLD ,15));
+        horizontalFrame22.add(answerTitle);
+        horizontalFrame23.add(fillerTitle);
+        System.out.println(answers);
+        for(int i = 0; i < answers.size(); i+=2){
+            horizontalFrame22.add(new JLabel(answers.get(i), SwingConstants.LEFT));
+            if(i+1 < answers.size()){
+                horizontalFrame23.add(new JLabel(answers.get(i+1), SwingConstants.LEFT));
+            }
+        }
+    }
+
+    public static void hideAnswer(){
+        horizontalFrame22.removeAll();
+        horizontalFrame23.removeAll();
+    }
+
     public static void sortArrayList(ArrayList<String> list){
         list.sort(Comparator.naturalOrder());
 
@@ -508,8 +609,9 @@ public class WordSearch extends JFrame implements ActionListener{
                         System.out.println(word);
                         word += String.valueOf(grid[i + direction[0] * counter][j + direction[1] * counter]);
                         if(searchFile(word.toLowerCase(), words, 0, FILELINES)){
-                            if(!answers.contains(word))
-                            answers.add(word);
+                            if(!answers.contains(word) && word.length() > 1){
+                                answers.add(word);
+                            }
                         }
                         counter++;
                     }
