a30fe899a46b11e8f2514b70560a556467da4fe3
Darren Liu <109255883+darren4141@users.noreply.github.com>
2024-05-14 09:55:32 -0400

    aninal java

---
+++ Lessons/Animal.java
@@ -0,0 +1,14 @@
+public class Animal {
+    private double size;
+    private String name;
+    private String species;
+    private String colour;
+    private int offspring;
+
+    public Animal(String name, String species){
+        this.name = name;
+        this.species = species;
+    }
+
+
+}
