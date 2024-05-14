99676e6655ca5cc7402de14849e3860f090be2df
Darren Liu <109255883+darren4141@users.noreply.github.com>
2024-05-14 10:48:41 -0400

    update

--- Lessons/TestAnimal.java
+++ Lessons/TestAnimal.java
@@ -5,5 +5,17 @@ public class TestAnimal {
 
     public static void main (String[] args){
         System.out.println(myAnimal.getName());
+
+        myAnimal.setName("tom");
+        System.out.println(myAnimal.getName());
+
+        System.out.println(myAnimal.getOffspring());
+        myAnimal.setOffspring(2);
+        System.out.println(myAnimal.getOffspring());
+        myAnimal.giveBirth(3);
+        System.out.println(myAnimal.getOffspring());
+
+        myAnimal.setAction("running");
+
     }
 }
