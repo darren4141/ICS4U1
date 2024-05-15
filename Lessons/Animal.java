a7f87ff05d3e53e41362d873b91345c327956353
Darren Liu <109255883+darren4141@users.noreply.github.com>
2024-05-15 08:40:28 -0400

    final

--- Lessons/Animal.java
+++ Lessons/Animal.java
@@ -1,20 +1,55 @@
+//====================================================================================================================================================================================================================================================================
+//
+//"OOP activity 2" Program
+//Darren Liu
+//May 14, 2024
+//Java 8
+//
+//====================================================================================================================================================================================================================================================================
+//	  
+//Problem definition:	Create an animal class with 5 behaviors and 5 attributes of animals
+//
+//	====================================================================================================================================================================================================================================================================
+
 package Lessons;
 public class Animal {
-    private double size = 0;
+    private double size;
     private String name;
     private String species;
     private String colour;
-    private int offspring = 0;
+    private int offspring;
+
+    public Animal(){
+        offspring = 0;
+        size = 0;
+        name = "default";
+        species = "default";
+        colour = "unknown";
+    }
 
     public Animal(String name, String species){
         this.name = name;
         this.species = species;
+        size = 0;
+        offspring = 0;
+        colour = "unknown";
     }
+
 
     public Animal(String name, String species, double size){
         this.name = name;
         this.species = species;
         this.size = size;
+        offspring = 0;
+        colour = "unknown";
+    }
+
+    public Animal(String name, String species, double size, String colour){
+        this.name = name;
+        this.species = species;
+        this.size = size;
+        offspring = 0;
+        this.colour = colour;
     }
 
     public void setOffspring(int offspring){
@@ -69,4 +104,15 @@ public class Animal {
         System.out.println(name + " the " + species + " is " + action);
     }
 
+    public void run(){
+        System.out.println(name + " the " + species + " is running!");
+    }
+
+    public void eat(){
+        System.out.println(name + " the " + species + " is eating!");
+    }
+
+    public void sleep(){
+        System.out.println(name + " the " + species + " is sleeping!");
+    }
 }
--- Lessons/TestAnimal.java
+++ Lessons/TestAnimal.java
@@ -1,7 +1,21 @@
+//====================================================================================================================================================================================================================================================================
+//
+//"OOP activity 2" Program
+//Darren Liu
+//May 14, 2024
+//Java 8
+//
+//====================================================================================================================================================================================================================================================================
+//	  
+//Problem definition:	Test out the animal class by creating two animal objects
+//
+//	====================================================================================================================================================================================================================================================================
+
 package Lessons;
 
 public class TestAnimal {
     static Animal myAnimal = new Animal("Jeff", "shark", 200);
+    static Animal myAnimal2 = new Animal();
 
     public static void main (String[] args){
         System.out.println(myAnimal.getName());
@@ -16,6 +30,13 @@ public class TestAnimal {
         System.out.println(myAnimal.getOffspring());
 
         myAnimal.setAction("running");
+
+        myAnimal2.eat();
+        myAnimal2.setColour("red");
+
+        System.out.println(myAnimal2.getColour());
+
+        System.out.println(myAnimal.getColour());
 
     }
 }
