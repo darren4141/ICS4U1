39d996361045a577be41790e874882f2ad57b14e
Darren Liu <109255883+darren4141@users.noreply.github.com>
2024-05-15 10:24:10 -0400

    polymorphcism

---
+++ Lessons/Polymorphism/Circle.java
@@ -0,0 +1,13 @@
+package Lessons.Polymorphism;
+
+public abstract class Circle extends Shape{
+    @Override
+    public void draw() {
+        System.out.println("This is a O");
+    }
+
+    @Override
+    public void erase() {
+        System.out.println("Erase the O shape");        
+    }
+}
---
+++ Lessons/Polymorphism/Shape.java
@@ -0,0 +1,6 @@
+package Lessons.Polymorphism;
+
+public abstract class Shape {
+    public abstract void draw();
+    public abstract void erase();
+}
